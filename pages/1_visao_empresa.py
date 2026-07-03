ec4e18b4c41443c74a4de8b82cdc94ffba1c6478
Karen Letícia <karenleticiaferreiradasilva@gmail.com>
2026-07-03 14:01:38 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -1,7 +1,7 @@
 # Bibliotecas
 import pandas as pd
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 from datetime import datetime
